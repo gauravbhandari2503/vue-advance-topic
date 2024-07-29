30bbe703795600356292a0a9b0a551da6354a096
gauravb2503 <gaurav.b@simformsolutions.com>
2024-07-29 12:00:34 +0530

    worked on vue advanced topics

--- src/components/DesignPattern/DesignPattern.vue
+++ src/components/DesignPattern/DesignPattern.vue
@@ -3,10 +3,40 @@
     <p>
         Creational, Structural, and Behavioral patterns are used to solve common problems in software design.
     </p>
-    <!-- <BuilderPattern /> -->
+    <ul>
+        <li>
+            Creational includes patterns like Singleton, Factory Method, Abstract Factory, Builder, Provider pattern etc.<br>
+        </li>
+        <li>
+            Structural includes patterns like Adapter, Bridge, Composite, Decorator, Facade, Flyweight, Proxy, and many more<br>
+        </li>
+        <li>
+            Behavioral includes patterns like Chain of Responsibility, Command, Interpreter, Iterator, Mediator, Memento, and Observer.<br>
+        </li>
+    </ul>
+    <hr>
+    <BuilderPattern />
+    <hr>
     <AdapterPattern />
+    <hr>
+    <div>
+        We have covered composable pattern which is a behavioral pattern. 
+        The composable pattern is a behavioral design pattern that promotes creating small, functional, reusable, and interchangeable building blocks, often referred to as composables.
+
+        We have also covered the Provider pattern in which we provide the implementation of Provide/Inject.
+
+    </div>
+    <hr>
+    <RouterLink :to="{path: '/observable-pattern'}">Go to Observalble pattern example</RouterLink>
 </template>
 <script lang="ts" setup>
 import BuilderPattern from './components/BuilderPattern.vue'
 import AdapterPattern from './components/AdapterPattern.vue'
 </script>
+
+<style>
+hr {
+    border: 2px solid black;
+    width: 100vh;
+}
+</style>
--- src/route/index.ts
+++ src/route/index.ts
@@ -1,9 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import ParentComponent from '../components/ParentComponent.vue'
 import ComposablePractise from '../components/ComposablePractise.vue'
-import IssueWithWatcher from '../components/IssueWithWatcher.vue'
 import PractiseReactivity from '../components/PractiseReactivity.vue'
 import DesignPattern from '../components/DesignPattern/DesignPattern.vue'
+import ObservablePattern from '../components/DesignPattern/components/Observable/ObservablePattern.vue'
+import ReactivityWithPinia from '../components/ReactivityWithPinia/ReactivityWithPinia.vue'
+import IssueWithWatcher from '../components/IssueWithWatcher.vue'
+import DynamicComponent from '../components/DynamicComponents/DynamicComponent.vue'
 
 const routes = [
   { path: '/', component: ParentComponent, name: 'home' },
@@ -11,6 +14,9 @@ const routes = [
   { path: '/issue-with-watcher', component: IssueWithWatcher, name: 'issue-with-watcher' },
   { path: '/practise-reactivity', component: PractiseReactivity, name: 'practise-reactivity' },
   { path: '/design-pattern', component: DesignPattern, name: 'design-pattern' },
+  { path: '/observable-pattern', component: ObservablePattern, name: 'observable-pattern' },
+  { path: '/reactivity-with-pinia', component: ReactivityWithPinia, name: 'reactivity-with-pinia' },
+  { path: '/dynamic-component', component: DynamicComponent, name: 'dynamic-component' },
 ]
 
 const router = createRouter({
